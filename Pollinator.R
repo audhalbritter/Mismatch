45173a77348a84762960ed291faf3c2b08cc71e8
Signe Svensson <Signe Svensson>
2016-10-14 14:34:52 +0200

    test

--- Pollinator.R
+++ Pollinator.R
@@ -14,6 +14,7 @@ Temperature <- Temperature %>%
   mutate(minutes = (floor(minute(date)/10)*10)) %>%
   mutate(date = ymd_hm(paste0(format(date, "%Y-%m-%d %H:"), minutes)))
 
+
 # Mean Temperature
 Temperature %>% 
   group_by(stage) %>% 
