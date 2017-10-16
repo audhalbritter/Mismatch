4d7f800ced2186ca5c5283299aba563a2e1382cc
audhalbritter <audhalbritter.com>
2017-10-16 08:39:58 +0200

    Function to draw maps And code to print maps as pdf

--- Maps.R
+++ Maps.R
@@ -1,11 +1,24 @@
-pheno2 <- phenology %>%
-  select(day, site, stage, plot, flowering)
-
-pollination %>% 
+# Combine phenology and pollination
+AllData <- pollination %>% 
   select(day, site, stage, fly) %>% 
-  full_join(pheno2, by = c("day", "site", "stage")) %>% 
-  gather(key = observation, value = value, -day, -site, -stage, -plot) %>% 
-  filter(year(day) == "2017", stage == "M") %>% 
-  ggplot(aes(x = day, y = value, colour = observation, shape = observation)) +
-  geom_point() +
-  facet_wrap(~ site)
+  full_join(phenology, by = c("day", "site", "stage")) %>% 
+  gather(key = observation, value = value, -day, -site, -stage)
+
+
+PhenoPollinationMap <- function(df){
+    ggplot(df, aes(x = day, y = value, color = observation, shape = observation)) + 
+      geom_point() +
+      facet_wrap(~ site) +
+      theme_minimal() +
+      ggtitle(unique(paste(df$stage, df$site, sep = " ")))
+  }
+  
+## plot maps
+Maps2017 <- AllData %>% 
+  filter(year(day) == "2017") %>% 
+  group_by(site, stage) %>% 
+  do(pheno.maps = PhenoPollinationMap(.))
+
+pdf(file = "Maps2017.pdf")
+Maps2017$pheno.maps
+dev.off()
