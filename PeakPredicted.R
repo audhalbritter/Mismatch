e47eee75779c97086933472f47b36e5dcf7def79
audhalbritter <audhalbritter.com>
2017-11-14 12:23:42 +0100

    change to sum flower to be able to run a glm fix PredictFlower function not using doy

--- PeakPredicted.R
+++ PeakPredicted.R
@@ -6,10 +6,9 @@ dat <- phenology %>%
   mutate(doy = yday(day))
 
 
-
-CompareModels <- function(dat){
-  fit1 <- glm(round(fl.sqm, 0) ~ doy + I(doy^2), data = dat, family = "poisson")
-  fit2 <- glm(round(fl.sqm, 0) ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+CompareModels.fl <- function(dat){
+  fit1 <- glm(flower.sum ~ doy + I(doy^2), data = dat, family = "poisson")
+  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
   tab <- AIC(fit1, fit2)
   AIC1 <- tab$AIC[1]
   AIC2 <- tab$AIC[2]
@@ -20,41 +19,49 @@ CompareModels <- function(dat){
 
 dat %>% 
   group_by(site, stage) %>%
-  do(CompareModels(.)) %>% 
+  do(CompareModels.fl(.)) %>% 
   mutate(Diff = AIC1 - AIC2) %>% pn
 
-plot(dat$doy, dat$fl.sqm)
-with(new.dat, lines(x = doy, y = exp(pred)), col = "red")
 
-
-tab <- AIC(fit1, fit2)
-tab$AIC[2]
-
-
-ggplot(dat, aes(x = doy, y = fl.sqm)) +
-  geom_point() +
-  geom_smooth()
-
-
-PredictFlower <- function(dat){
-  fit2 <- glm(round(fl.sqm, 0) ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-  new.dat <- data.frame(doy = seq(min(dat$doy), max(dat$doy), length.out = 30))
+PredictPeakFlower <- function(dat){
+  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+  new.dat <- data.frame(doy = dat$doy)
   new.dat$pred <- exp(predict(fit2, new.dat))
   new.dat$pred
-  res <- data_frame(pred = new.dat$pred)
+  res <- data_frame(doy = new.dat$doy,
+                    pred = new.dat$pred)
   return(res)
 }
 
 pred.fl <- dat %>%
-  group_by(site, stage, doy) %>%
-  do(PredictFlower(.))
+  group_by(site, stage) %>%
+  do(PredictPeakFlower(.))
 
-
+# Calculate peak, first and end
 PredFl <- pred.fl %>%
   group_by(site, stage) %>%
   summarize(first = first(doy), peak = doy[which.max(pred)], last = last(doy)) %>% 
   rename(first.fl = first, peak.fl = peak, last.fl = last)
 
+
+#********************************************************************************************
+### Test one site and stage
+plot(dat$doy, dat$flower.sum)
+with(new.dat, lines(x = doy, y = pred), col = "red")
+
+fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+new.dat <- data.frame(doy = dat$doy)
+new.dat$pred <- exp(predict(fit2, new.dat))
+new.dat
+
+plot(dat$doy, dat$flowering)
+with(new.dat, lines(x = doy, y = exp(pred)), col = "red")
+
+pred.fl %>% 
+  filter(site == "07", stage == "F") %>% 
+  ggplot(aes(x = doy, y = pred)) +
+  geom_point()
+#********************************************************************************************
 
 
 ##### INSECTS
@@ -65,42 +72,36 @@ dat <- pollination %>%
   mutate(poll.sqm.trans = (poll.sqm - min(poll.sqm))/(max(poll.sqm) - min(poll.sqm)))
 
 
-Compare.models <- function(dat){
-fit2 <- glm(round(poll.sqm*100, 0) ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-  tab <- AIC(fit1, fit2)
-  AIC1 <- tab$AIC[1]
-  AIC2 <- tab$AIC[2]
-  res <- data_frame(AIC1 = AIC1,
+Compare.models.pol <- function(dat){
+    fit1 <- glm(fly ~ doy + I(doy^2), data = dat, family = "poisson")
+    fit2 <- glm(fly ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+    tab <- AIC(fit1, fit2)
+    AIC1 <- tab$AIC[1]
+    AIC2 <- tab$AIC[2]
+    res <- data_frame(AIC1 = AIC1,
                     AIC2 = AIC2)
   return(res)
 }
 
 dat %>% 
   group_by(site, stage) %>%
-  do(Compare.models(.)) %>% 
+  do(Compare.models.pol(.)) %>% 
   mutate(Diff = AIC1 - AIC2) %>% pn
 
 
 
-plot(dat$doy, dat$poll.sqm*100)
-with(new.dat, lines(x = doy, y = exp(pred)), col = "red")
-new.dat <- data.frame(doy = seq(min(dat$doy), max(dat$doy), length.out = 30))
-new.dat$pred <- predict(fit2, new.dat)
-exp(new.dat$pred)/100
-
-
-
 PredictPollinator <- function(dat){
-  fit2 <- glm(round(poll.sqm*100, 0) ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-  new.dat <- data.frame(doy = seq(min(dat$doy), max(dat$doy), length.out = 30))
+  fit2 <- glm(fly ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+  new.dat <- data.frame(doy = dat$doy)
   new.dat$pred <- exp(predict(fit2, new.dat))
   new.dat$pred
-  res <- data_frame(pred = new.dat$pred)
+  res <- data_frame(doy = new.dat$doy,
+                    pred = new.dat$pred)
   return(res)
 }
 
 pred.poll %>% 
-  group_by(site, stage, doy) %>%
+  group_by(site, stage) %>%
   do(PredictPollinator(.))
 
 
@@ -121,7 +122,4 @@ AllPred$siteID <- as.character(AllPred$siteID)
 AllPred$siteID <- factor(AllPred$siteID, levels=unique(AllPred$siteID))  
 
 
-# PLOTTING PEAK DIFF AGAINST SITE
-ggplot(AllPred, aes(y = peak.diff, x = siteID)) +
-  geom_point() +
-  theme_minimal()
+ 
