78d21d5a202f328152c9726828a1c0287d1de0ec
SiljeOstman <silje.ostman@gmail.com>
2017-11-14 14:35:33 +0100

    New predicted values for flowers and pollinators, and a new dataset combining first, peak and last for flowers and pollinators

--- PeakPredicted.R
+++ PeakPredicted.R
@@ -1,14 +1,14 @@
 ##### FLOWERS
 
-dat <- phenology %>% 
+dat.fl <- phenology %>% 
   filter(year == "2017") %>% 
   #filter(site == "01", stage == "F") %>% 
   mutate(doy = yday(day))
 
 
-CompareModels.fl <- function(dat){
-  fit1 <- glm(flower.sum ~ doy + I(doy^2), data = dat, family = "poisson")
-  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+CompareModels.fl <- function(dat.fl){
+  fit1 <- glm(flower.sum ~ doy + I(doy^2), data = dat.fl, family = "poisson")
+  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat.fl, family = "poisson")
   tab <- AIC(fit1, fit2)
   AIC1 <- tab$AIC[1]
   AIC2 <- tab$AIC[2]
@@ -17,23 +17,23 @@ CompareModels.fl <- function(dat){
   return(res)
 }
 
-dat %>% 
+dat.fl %>% 
   group_by(site, stage) %>%
   do(CompareModels.fl(.)) %>% 
   mutate(Diff = AIC1 - AIC2) %>% pn
 
 
-PredictPeakFlower <- function(dat){
-  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-  new.dat <- data.frame(doy = dat$doy)
-  new.dat$pred <- exp(predict(fit2, new.dat))
-  new.dat$pred
-  res <- data_frame(doy = new.dat$doy,
-                    pred = new.dat$pred)
+PredictPeakFlower <- function(dat.fl){
+  fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat.fl, family = "poisson")
+  new.dat.fl <- data.frame(doy = dat.fl$doy)
+  new.dat.fl$pred <- exp(predict(fit2, new.dat.fl))
+  new.dat.fl$pred
+  res <- data_frame(doy = new.dat.fl$doy,
+                    pred = new.dat.fl$pred)
   return(res)
 }
 
-pred.fl <- dat %>%
+pred.fl <- dat.fl %>%
   group_by(site, stage) %>%
   do(PredictPeakFlower(.))
 
@@ -46,16 +46,16 @@ PredFl <- pred.fl %>%
 
 #********************************************************************************************
 ### Test one site and stage
-plot(dat$doy, dat$flower.sum)
-with(new.dat, lines(x = doy, y = pred), col = "red")
+plot(dat.fl$doy, dat.fl$flower.sum)
+with(new.dat.fl, lines(x = doy, y = pred), col = "red")
 
-fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-new.dat <- data.frame(doy = dat$doy)
-new.dat$pred <- exp(predict(fit2, new.dat))
-new.dat
+fit2 <- glm(flower.sum ~ doy + I(doy^2) + I(doy^3), data = dat.fl, family = "poisson")
+new.dat.fl <- data.frame(doy = dat.fl$doy)
+new.dat.fl$pred <- exp(predict(fit2, new.dat.fl))
+new.dat.fl
 
-plot(dat$doy, dat$flowering)
-with(new.dat, lines(x = doy, y = exp(pred)), col = "red")
+plot(dat.fl$doy, dat.fl$flowering)
+with(new.dat.fl, lines(x = doy, y = exp(pred)), col = "red")
 
 pred.fl %>% 
   filter(site == "07", stage == "F") %>% 
@@ -65,16 +65,16 @@ pred.fl %>%
 
 
 ##### INSECTS
-dat <- pollination %>% 
+dat.pol <- pollination %>% 
   filter(year == "2017") %>% 
   #filter(site == "01", stage == "F") %>% 
-  mutate(doy = yday(day)) %>% 
-  mutate(poll.sqm.trans = (poll.sqm - min(poll.sqm))/(max(poll.sqm) - min(poll.sqm)))
+  mutate(doy = yday(day)) 
+  #mutate(poll.sqm.trans = (poll.sqm - min(poll.sqm))/(max(poll.sqm) - min(poll.sqm)))
 
 
-Compare.models.pol <- function(dat){
-    fit1 <- glm(fly ~ doy + I(doy^2), data = dat, family = "poisson")
-    fit2 <- glm(fly ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
+Compare.models.pol <- function(dat.pol){
+    fit1 <- glm(fly ~ doy + I(doy^2), data = dat.pol, family = "poisson")
+    fit2 <- glm(fly~ doy + I(doy^2) + I(doy^3), data = dat.pol, family = "poisson")
     tab <- AIC(fit1, fit2)
     AIC1 <- tab$AIC[1]
     AIC2 <- tab$AIC[2]
@@ -83,28 +83,28 @@ Compare.models.pol <- function(dat){
   return(res)
 }
 
-dat %>% 
+dat.pol %>% 
   group_by(site, stage) %>%
   do(Compare.models.pol(.)) %>% 
   mutate(Diff = AIC1 - AIC2) %>% pn
 
 
 
-PredictPollinator <- function(dat){
-  fit2 <- glm(fly ~ doy + I(doy^2) + I(doy^3), data = dat, family = "poisson")
-  new.dat <- data.frame(doy = dat$doy)
-  new.dat$pred <- exp(predict(fit2, new.dat))
-  new.dat$pred
-  res <- data_frame(doy = new.dat$doy,
-                    pred = new.dat$pred)
+PredictPollinator <- function(dat.pol){
+  fit2 <- glm(fly ~ doy + I(doy^2) + I(doy^3), data = dat.pol, family = "poisson")
+  new.dat.pol <- data.frame(doy = dat.pol$doy)
+  new.dat.pol$pred <- exp(predict(fit2, new.dat.pol))
+  new.dat.pol$pred
+  res <- data_frame(doy = new.dat.pol$doy,
+                    pred = new.dat.pol$pred)
   return(res)
 }
 
-pred.poll %>% 
+pred.poll <- dat.pol %>% 
   group_by(site, stage) %>%
   do(PredictPollinator(.))
 
-
+# Calculate peak, first and end
 PredPoll <- pred.poll %>%
   group_by(stage, site) %>%
   summarize(first = first(doy), peak = doy[which.max(pred)], last = last(doy)) %>% 
@@ -116,10 +116,7 @@ PredPoll <- pred.poll %>%
 
 AllPred <- PredPoll %>% 
   left_join(PredFl, by=c("stage"="stage", "site"="site")) %>% 
-  mutate(peak.diff = peak.fl-peak.poll, siteID = paste(stage, site)) %>% 
+  mutate(peak.diff = peak.fl-peak.poll, siteID = paste(stage, site))
 
 AllPred$siteID <- as.character(AllPred$siteID)
 AllPred$siteID <- factor(AllPred$siteID, levels=unique(AllPred$siteID))  
-
-
- 
