82c5340063f4f6e8f8ae80d36d21fdc7f6351f4a
SiljeOstman <silje.ostman@gmail.com>
2017-11-16 13:36:28 +0100

    .

--- PeakPredicted.R
+++ PeakPredicted.R
@@ -116,7 +116,7 @@ PredPoll <- pred.poll %>%
 
 AllPred <- PredPoll %>% 
   left_join(PredFl, by=c("stage"="stage", "site"="site")) %>% 
-  mutate(peak.diff = peak.fl-peak.poll, siteID = paste(stage, site))
+  mutate(peak.diff = peak.fl-peak.poll, siteID = paste(stage, site)) %>% 
 
 AllPred$siteID <- as.character(AllPred$siteID)
 AllPred$siteID <- factor(AllPred$siteID, levels=unique(AllPred$siteID))  
