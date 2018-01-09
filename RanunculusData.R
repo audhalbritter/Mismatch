632b4c81b98cd3115d14b98330c99f939a99cace
audhalbritter <audhalbritter.com>
2018-01-09 20:09:24 +0100

    Fix site (01, 02,...) in biomass16
    add siteID to phenology, pollination, pollination2 and Biomass

--- RanunculusData.R
+++ RanunculusData.R
@@ -148,7 +148,8 @@ phenology <- pheno16 %>%
   bind_rows(pheno17) %>% 
   # fix weather !!!
   mutate(stage = factor(stage, levels = c("F", "E", "M", "L"))) %>% 
-  group_by(day, stage, site, year) %>% 
+  mutate(siteID = paste(stage, site, sep = " ")) %>% 
+  group_by(day, stage, site, year, siteID) %>% 
   summarise(flower.sum = sum(flowering), flower.mean = mean(flowering)) %>% 
   mutate(fl.sqm = flower.mean*2)
 
@@ -160,6 +161,7 @@ pollination <- pollination16 %>%
   # add climate data
   #left_join(Temperature, by = c("date" = "date", "stage" = "stage", "site" = "site"))
   mutate(stage = factor(stage, levels = c("F", "E","M", "L"))) %>%
+  mutate(siteID = paste(stage, site, sep = " ")) %>% 
   mutate(weather = factor(weather, levels = c("sun", "sun_cloud","cloud_sun", "cloud"))) %>% 
   mutate(poll.sqm = fly/area)
 
@@ -192,6 +194,7 @@ biomass16 <- biomass16 %>%
   rename(Treatment = Plant, Biomass = `Vekt biomasse`, Seed_mass = `Vekt frø`, Seed_number = `Antall frø`, Ovule_number = `Antall ovuler`, Date1 = `Dato pollinert 1`, Date2 = `Dato pollinert 2`, Date3 = `Dato pollinert 3`, Name1 = Hvem1, Name2 = Hvem2, Name3 = Hvem3, Collected = `Dato samlet frø`, NameCollected = Hvem4) %>%
   mutate(Stage = gsub("^([A-Z]).*", "\\1", Plot)) %>% 
   mutate(Site = gsub(".(\\d+).", "\\1", Plot)) %>% 
+  mutate(Site = ifelse(nchar(Site) < 2, paste(0, Site, sep = ""), Site)) %>% # add a zero to Site
   mutate(Block = gsub(".*([a-z])$", "\\1", Plot)) %>% 
   mutate(Treatment = ifelse(Treatment %in% c("C1", "C2"), "Control", "Pollinated")) %>%
   mutate(Biomass = as.numeric(Biomass), Seed_mass = as.numeric(Seed_mass)) %>%
@@ -208,6 +211,7 @@ Biomass17 <- read_excel("Data/2017/Biomass.xlsx", col_types = c("text", "text", 
 Biomass17 <- Biomass17 %>% 
   rename(Treatment = Plant_type, Date1 = `Date  1`, Date2 = `Date 2`, Date3 = `Date 3`, Name1 = `Name 1`, Name2 = `Name 2`, Name3 = `Name 3`, siteID = `Site`) %>%
   mutate(Treatment = ifelse(Treatment %in% c("C"), "Control", "Pollinated")) %>% 
+  mutate(Site = substr(siteID, 3, 4)) %>% 
   mutate(Year = 2017)
 
 Biomass <- biomass16 %>% 
@@ -221,7 +225,7 @@ Biomass <- biomass16 %>%
 
 # Find closest phenology observation to each pollination observation
 pollination2 <- pollination %>% 
-  full_join(phenology, by = c("site", "stage"), suffix = c(".poll",".fl")) %>% 
+  full_join(phenology, by = c("site", "stage", "siteID"), suffix = c(".poll",".fl")) %>% 
   select(-weather, -wind, -remark) %>% 
   mutate(diff = day.poll - day.fl, abs.diff = abs(diff)) %>% 
   mutate(abs.diff.mult = if_else(diff > 0, abs.diff * 1.9, abs.diff)) %>% 
