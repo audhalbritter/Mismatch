6b0ad866e7c664ce719bae76d474bd3ec3717fd5
SiljeOstman <silje.ostman@gmail.com>
2018-01-26 16:00:53 +0100

    Testing reproductive output by treatment and stage

--- stats_Silje.R
+++ stats_Silje.R
@@ -13,7 +13,7 @@ peaks.17 <- AllPred %>%
   filter(year == 2017)
 
 summary(lm(peak.fl~peak.poll, peaks.17))
-anova(lm(peak.fl~peak.poll, peaks.17))
+summary(lm(peak.fl~peak.poll*stage, peaks.17))
 
 ##### MISMATCH ~ STAGE #############################################################
 # 2016
@@ -30,26 +30,47 @@ stage_mismatch17 <- AllPred %>%
 summary(lm(peak.diff ~ stage, stage_mismatch17))
 
 
-##### REPROD.OUTPUT ~ TREATMENT ####################################################
-#2016
+##### REPROD.OUTPUT####################################################
+# 2016
+# Output of treatments
 treat16 <- Biomass %>% 
   filter(Year==2016, Stage != "L")
 summary(lm(Seed_mass ~ Treatment, treat16))
 
+# Output of treatments and stage
+treat16 <- Biomass %>% 
+  filter(Year==2016, Stage != "L")
+summary(lm(Seed_mass ~ Treatment*Stage, treat16))
 
+# Output of controls per stage
 contr16 <- Biomass %>% 
   filter(Year==2016, Stage != "L", Treatment == "Control")
 summary(lm(Seed_mass ~ Stage, contr16))
 
-#2017
+
+# 2017
+# Output of treatments
 treat17 <- Biomass %>% 
   filter(Year == 2017)
 summary(lm(Seed_mass ~ Treatment, treat17))
 
+# Output of treatments and stage
 treat17 <- Biomass %>% 
+  filter(Year == 2017)
+summary(lm(Seed_mass ~ Treatment*Stage, treat17))
+
+# Output of controls per stage
+contr17 <- Biomass %>% 
   filter(Year == 2017, Treatment=="Control")
-summary(lm(Seed_mass ~ Stage, treat17))
-anova(lm(Seed_mass ~ Stage, treat17))
+summary(lm(Seed_mass ~ Stage, contr17))
+anova(lm(Seed_mass ~ Stage, contr17))
+
+
+
+# treatment*stage 2016
+
+
+# treatment*stage 2017
 
 
 ##### REPROD.OUTPUT ~ MISMATCH #####################################################
