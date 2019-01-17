605fe7b90799c66cadd4f38b97dd20cb334d2be3
linnvassvik <linnvassvik@gmail.com>
2019-01-17 13:46:17 +0100

    Endret på tekstbeskrivelsen til analysene, ingenting viktig

--- AnalyseLinn.R
+++ AnalyseLinn.R
@@ -12,7 +12,7 @@ Biomass <- read.csv("Biomass1617.csv", sep=";") %>%
   mutate(BlockID = as.factor(paste(Stage, Site, Block))) %>% #gir block en mer presis id
   filter(!is.na(Seed_mass)) %>%  #fjerner alle NA 
   filter(!is.na(Biomass)) %>%
-  filter(!is.na(Tot_ovule)) #fjerner alle NA
+  #filter(!is.na(Tot_ovule)) #fjerner alle NA
  
   
 
@@ -27,7 +27,7 @@ tidy(Model) %>%
   mutate(estimate = exp(estimate))
 
 
-#####Grafer med biomasse som variabel#######
+#####Grafer med biomasse som variabel, og AIC test#######
 
 ############################################
 #Graf med biomasse og frøvekt. Ser på hvert år hver for seg
@@ -49,16 +49,16 @@ ggplot(Biomass, aes(x = Biomass, y = Ovule_number, color = Stage)) +
   geom_smooth(method = "lm") +
   facet_wrap(~ Year)
 
-#Model med random effects. Endret kode fra biomasse x frøvekt. Kan kun bruke lme om dataene er normalfordelt, bruker derfor glme her, og legger inn poisson siden det er telledata. Også endret form på random effekts, fordi "random = ~ 1 | BlockID" ikke fungerer med "glmer".
+#Model med random effects. Endret kode fra biomasse x frøvekt. Kan kun bruke lme om dataene er normalfordelt, men må bruke glme her. Legger inn family = poisson siden det er telledata. Også endret form på random effekts, fordi "random = ~ 1 | BlockID" ikke fungerer med "glmer".
 ModelOvule0 <- glmer(Ovule_number ~ Biomass + (1 | BlockID), family="poisson", data = Biomass %>% filter(Year == 2016)) 
 ModelOvule1 <- glmer(Ovule_number ~ Stage + (1 | BlockID), family="poisson", data = Biomass %>% filter(Year == 2016)) 
 ModelOvule2 <- glmer(Ovule_number ~ Biomass+Stage + (1 | BlockID), family="poisson", data = Biomass %>% filter(Year == 2016)) 
 ModelOvule3 <- glmer(Ovule_number ~ Biomass*Stage + (1 | BlockID), family="poisson", data = Biomass %>% filter(Year == 2016)) 
 summary(ModelOvule0)
 
-#Kan gjøre AIC test her for å se om modellen vi valgte over er den beste (?)
+#Kan gjøre AIC test her for å se hvilken modell som er den beste
 AIC(ModelOvule0, ModelOvule1, ModelOvule2, ModelOvule3)
-#Modell 0 har lavest verdi og er den som forklarer mest, altså biomassen sier mer om antall ovuler enn stage og intraksjon mellom biomasse og stage (?). Begge verdiene er signifikante
+#Modell 0 har lavest verdi og er den som forklarer mest, altså biomassen sier mer om antall ovuler enn stage og intraksjon mellom biomasse og stage (?). Både antall ovule og biomasse er signifikante
 
 #######################################################
 # Graf med biomasse og antall frø. Ser på år hver for seg
@@ -75,8 +75,7 @@ ModelSeed3 <- glmer(Seed_number ~ Biomass*Stage + (1 | BlockID), family = "poiss
 summary(ModelSeed3)
 
 AIC(ModelSeed0, ModelSeed1, ModelSeed2, ModelSeed3)
-#Model nr 3 er den som har lavest verdi, slik at interaksjonen mellom biomasse og stage forklarer best resultatet vi ser?
-#Resultatet sier at biomassen har litt å si på hvor mange frø som blir produsert. Stage E og L er signifikante
+#Model nr 3 er den som har lavest verdi, slik at interaksjonen mellom biomasse og stage forklarer best resultatet vi ser (?).
 
 ##################################################
 #Graf med antall frø + antall ovule og hvordan biomasse påvirker her
@@ -94,7 +93,7 @@ summary(ModelSeedOvule2)
 
 #AIC test
 AIC(ModelSeedOvule0, ModelSeedOvule1, ModelSeedOvule2, ModelSeedOvule3)
-#Model 2 har lavest AIC verdi og derfor den beste modellen. Den forteller oss at stage L har høyest verdi av frø+ovule. Og stage M er signifikant
+#Model 2 har lavest AIC verdi og derfor den beste modellen. Den forteller oss at stage L har høyest verdi av frø+ovule. 
 
 ##################################################
 #Graf med antall frø/(antall frø + antall ovule) og hvordan biomasse påvirker her. Hvordan lage denne grafen?
@@ -110,9 +109,20 @@ ModelSeedset2 <- glmer(Seed_number ~ Biomass+Stage + (1 | BlockID) + offset(log(
 ModelSeedset3 <- glmer(Seed_number ~ Biomass*Stage + (1 | BlockID) + offset(log(Tot_ovule)), family="poisson", data = Biomass %>% filter(Year == 2016)) 
 summary(ModelSeedset1)
 
+# OBS! får ikke helt til å fjerne NA fra Tot_ovule i linje 15, må fikse dette for å kjøre formel
+
 AIC(ModelSeedset0, ModelSeedset1, ModelSeedset2, ModelSeedset3)
 #Modell 1 har lavest verdi. Resultatene viser at stage M har flest frø produsert ut i fra mulig utgangspunkt, og stage L har lavest mengde produsert frø. Både stage E og L er signifikante.
 
+###################################################
+#Korrelasjonstester, går det ann å bare bruke seedmass, eller har antall frø og ovuler noe å si?
+
+cor.test(Biomass$Seed_mass, Biomass$Seed_number)
+
+plot(Biomass$Seed_mass, Biomass$Seed_number)
+plot(Biomass$Seed_mass, Biomass$Ovule_number)
+#plot(Biomass$Seed_mass, Biomass$Tot_ovule) får ikke helt til å fjerne NA fra Tot_ovule i linje 15, må fikse dette.
+#Virker som antall frø og frømasse er greit korrelert (?), men ikke når vi ser på antall ovuler og frømasse.
 
 ################################################################################
 # Funket ikke helt, ser på senere
