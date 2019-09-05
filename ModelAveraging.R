d7aa1a7200a969f26d5e5f748781fa9c6f1ab988
linnvassvik <linnvassvik@gmail.com>
2019-09-05 18:53:12 +0200

    bare slettet noen koder jeg knotet litt med selv

--- ModelAveraging.R
+++ ModelAveraging.R
@@ -246,40 +246,6 @@ Seedmass2017 <- ggplot(resSM173, aes(x = Estimate, y = Variable)) +
   geom_vline(xintercept = 0, linetype="dashed", color = "grey")
 ggsave(Seedmass2017, filename = "Figurer/Seedmass2017.jpeg", height = 6, width = 8)
 
-###############################
-#Model with interaction????
-#Define the model
-Model2SeedMass2016 <- lmer(log(Seed_mass) ~ Biomass * Stage + Treatment + MeanFlower.cen + CumTemp.cen + CumPrec.cen + (1|BlockID), data = dat2016, REML = FALSE)
-
-#Plot
-fix.check(Model2SeedMass2016)
-plot(Model2SeedMass2016)
-
-#Apply dredge function
-model2.setSM16 <- dredge(Model2SeedMass2016, rank = "AICc", extra = "R^2")
-
-#Make a new dataframe
-mm2SM16 <- data.frame(model2.setSM16)
-mm2SM16$cumsum <- cumsum(mm2SM16$weight)
-mm2SM16
-
-#Sekect the 95% confidence interval
-mm2SM1695 <- mm2SM16 %>% filter(cumsum < 0.95)
-mm2SM1695 
-#Gives us 28 models
-
-#Model averaging based on AIC values
-averaged.model2SM16 <- model.avg(model2.setSM16, cumsum(weight) <= 0.95)
-averaged.model2SM16
-
-#Getting the table to present
-res2SM16 <- data.frame(summary(averaged.model2SM16)$coefmat.full)
-res2SM16
-
-summary(ModelSeedMass2016)
-
-
-
 
 
 
